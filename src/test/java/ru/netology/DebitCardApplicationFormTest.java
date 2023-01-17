2ed5e87f6490a3fd8b7e62d1d2f8db0784e264ce
Tatyana Fomina <tatyana24fomina@gmail.com>
2023-01-17 23:26:41 +0400

    доработан тест

--- src/test/java/ru/netology/DebitCardApplicationFormTest.java
+++ src/test/java/ru/netology/DebitCardApplicationFormTest.java
@@ -1,7 +1,6 @@
 package ru.netology;
 
 import com.codeborne.selenide.Condition;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.$;
@@ -14,13 +13,14 @@ public class DebitCardApplicationFormTest {
         String fullName = "Иванов Сергей Петрович";
         String number = "+79996665544";
         open("http://localhost:9999/");
-        $("#root > div > form > div:nth-child(1) > span > span > span.input__box > input").setValue(fullName);
-        $("#root > div > form > div:nth-child(2) > span > span > span.input__box > input").setValue(number);
-        $("#root > div > form > div:nth-child(3) > label > span.checkbox__box").click();
-        $("#root > div > form > div:nth-child(4) > button").click();
+        //$("#root > div > form > div:nth-child(1) > span > span > span.input__box > input").setValue(fullName);
+        $("[data-test-id='name'] input").sendKeys(fullName);
+        $("[data-test-id='phone'] input").sendKeys(number);
+        $("[data-test-id='agreement'] span").click();
+        $("button").click();
 
         String expected = "  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
-        $("#root > div > div > p").shouldHave(Condition.text(expected));
+        $("[data-test-id='order-success'] ").shouldHave(Condition.visible);
 
     }
 
